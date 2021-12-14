5a8ebc03d3ad55613aa693ebb76de3116924920d
loren <lorenmichaelh@gmail.com>
2021-12-13 21:24:13 -0600

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    manageToDo(e.target.newListItem.value);
+    form.reset();
+  })
 });
+
+function manageToDo(todo) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = ' X ';
+  p.textContent = `${todo}  `;
+  p.appendChild(btn);
+  document.querySelector('#tasks').appendChild(p);
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
